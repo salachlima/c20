f788d27e11318e0864d373c9c2521c77a1727e67
salachlima <75402511+salachlima@users.noreply.github.com>
2020-12-14 20:51:49 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+/*var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+  movingRect = createSprite(400, 400, 80, 30);
+  movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background("black");
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;  
+  console.log(movingRect.y - fixedRect.y);
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  drawSprites();
+}*/
+
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background("black");
+  
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+  && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+  movingRect.velocityX = movingRect.velocityX * (-1);
+  fixedRect.velocityX = fixedRect.velocityX * (-1);
+}
+if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+  && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+  movingRect.velocityY = movingRect.velocityY * (-1);
+  fixedRect.velocityY = fixedRect.velocityY * (-1);
+}
+  drawSprites();
+}
